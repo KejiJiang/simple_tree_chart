f7c19ad042d65abda977890b5ec759a34ecc78f1
KejiJiang <2403518249@qq.com>
2024-05-27 11:21:18 +0800

    简易树形图谱

---
+++ new.js
@@ -0,0 +1,346 @@
+const data = {
+    name: "Major",
+    children: [
+      {
+        name: "Paramount",
+        children: [
+          {
+            name: "Norbit",
+          },
+          {
+            name: "Bee Movie",
+          },
+          {
+            name: "Beowulf",
+          },
+          {
+            name: "The Heartbreak Kid",
+          },
+          {
+            name: "The Kite Runner",
+          },
+          {
+            name: "Transformers",
+          },
+          {
+            name: "Next",
+          },
+          {
+            name: "Shooter",
+          },
+          {
+            name: "Disturbia",
+          },
+          {
+            name: "Zodiac",
+          },
+          {
+            name: "No Country for Old Men",
+          },
+          {
+            name: "Shrek the Third",
+          },
+          {
+            name: "Stardust",
+          },
+          {
+            name: "Into the Wild",
+          },
+          {
+            name: "Blades of Glory",
+          },
+          {
+            name: "There Will Be Blood",
+          },
+        ]
+      },
+      {
+        name: "Sony",
+        children: [
+          {
+            name: "Daddy Day Camp",
+          },
+          {
+            name: "Walk Hard: The Dewey Cox Story",
+          },
+          {
+            name: "Superbad",
+          },
+          {
+            name: "Surf's Up",
+          },
+          {
+            name: "Spider-Man 3",
+          },
+          {
+            name: "Ghost Rider",
+          },
+          {
+            name: "Vacancy",
+          },
+          {
+            name: "We Own the Night",
+          },
+          {
+            name: "Premonition",
+          },
+        ]
+      },
+      {
+        name: "The Weinstein Company",
+        children: [
+          {
+            name: "The Nanny Diaries",
+          },
+          {
+            name: "The Mist",
+          },
+          {
+            name: "Halloween",
+          },
+          {
+            name: "Grindhouse",
+          },
+          {
+            name: "Sicko",
+          },
+        ]
+      },
+      {
+        name: "Universal",
+        children: [
+          {
+            name: "I Now Pronounce You Chuck and Larry",
+          },
+          {
+            name: "Evan Almighty",
+          },
+          {
+            name: "Charlie Wilson's War",
+          },
+          {
+            name: "Knocked Up",
+          },
+          {
+            name: "Smokin' Aces",
+          },
+          {
+            name:"American Gangster",
+          },
+          {
+            name:"The Bourne Ultimatum",
+          },
+        ]
+      },
+      {
+        name: "Warner Bros.",
+        children:[
+          {
+            name:"The Bucket List",
+          },
+          {
+            name:"License to Wed",
+          },
+          {
+            name:"Music and Lyrics",
+          },
+          {
+            name:"The Invasion",
+          },
+          {
+            name:"Shoot 'Em Up",
+          },
+          {
+            name:"I Am Legend",
+          },
+          {
+            name:"Harry Potter and the Order of the Phoenix",
+          },
+          {
+            name:"Rush Hour 3",
+          },
+          {
+            name:"The Golden Compass",
+          },
+          {
+            name:"Ocean's 13",
+          },
+          {
+            name:"Sweeney Todd: The Demon Barber of Fleet Street",
+          },
+          {
+            name:"300",
+          },
+          {
+            name:"In the Land of Women",
+          },
+          {
+            name:"Hairspray",
+          },
+        ]
+      },
+      {
+        name: "(null)",
+        children:[
+          {
+            name:"No Reservations",
+          },
+          {
+            name:"TMNT",
+          },
+          {
+            name:"The Reaping",
+          },
+          {
+            name:"The Kingdom",
+          },
+          {
+            name:"Pathfinder: Legend of the Ghost Warrior",
+          },
+          {
+            name:"The Brave One",
+          },
+          {
+            name:"Average",
+          },
+        ]
+      },
+    ],
+  };
+  const ldata = {
+    name: "Major Studio",
+    children: [
+      {
+        name: "Disney",
+        children: [
+          {
+            name: "The Game Plan",
+          },
+          {
+            name: "Enchanted",
+          },
+          {
+            name: "Meet the Robinsons",
+          },
+          {
+            name: "Pirates of the Caribbean: At World's End",
+          },
+          {
+            name: "National Treasure: Book of Secrets",
+          },
+          {
+            name: "Wild Hogs",
+          },
+          {
+            name: "Ratatouille",
+          },
+        ]
+      },
+      {
+        name: "Fox",
+        children: [
+          {
+            name: "Alvin and the Chipmunks",
+          },
+          {
+            name: "The Darjeeling Limited",
+          },
+          {
+            name: "The Simpsons Movie",
+          },
+          {
+            name: "Juno",
+          },
+          {
+            name: "Alien vs. Predator -- Requiem",
+          },
+          {
+            name: "Fantastic Four: Rise of the Silver Surfer",
+          },
+          {
+            name: "Hitman",
+          },
+          {
+            name: "Live Free or Die Hard",
+          },
+        ]
+      },
+      {
+        name: "Independent",
+        children: [
+          {
+            name: "Epic Movie",
+          },
+          {
+            name: "Saw IV",
+          },
+          {
+            name: "P.S. I Love You",
+          },
+          {
+            name: "Tyler Perry's Why Did I get Married",
+          },
+          {
+            name: "Across the Universe",
+          },
+          {
+            name: "Waitress",
+          },
+          {
+            name: "Stomp the Yard",
+          },
+          {
+            name: "The Hills Have Eyes 2",
+          },
+          {
+            name: "30 Days of Night",
+          },
+          {
+            name: "Lions for Lambs",
+          },
+          {
+            name: "Nancy Drew",
+          },
+          {
+            name: "Mr. Magorium's Wonder Emporium",
+          },
+          {
+            name: "Michael Clayton",
+          },
+        ]
+      },
+      {
+        name: "Lionsgate",
+        children: [
+          {
+            name: "Hostel: Part II",
+          },
+          {
+            name: "Good Luck Chuck",
+          },
+          {
+            name: "Happily N'Ever After",
+          },
+          {
+            name: "3:10 to Yuma",
+          },
+        ]
+      },
+      {
+        name: "Mediaplex",
+        children: [
+          {
+            name: "Dragon Wars (aka D-War)",
+          },
+        ]
+      },
+      {
+        name: "MGM",
+        children: [
+          {
+            name: "Mr. Brooks",
+          },
+        ]
+      },
+    ],
+  };
